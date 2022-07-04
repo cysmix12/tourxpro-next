7d97d3935ca40920f6b9e062d96ff03b54a6fe2c
cysmix12 <rjsocs@gmail.com>
2022-07-05 07:34:23 +0800

    Added overall title in Head

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,18 @@
 import SimpleReactLightbox from 'simple-react-lightbox';
+import Head from 'next/head';
 
 import '../src/index.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <SimpleReactLightbox>
-      <Component {...pageProps} />
-    </SimpleReactLightbox>
+    <>
+      <Head>
+        <title>TourXPro - Travels Tourism Agency</title>
+      </Head>
+      <SimpleReactLightbox>
+        <Component {...pageProps} />
+      </SimpleReactLightbox>
+    </>
   );
 }
 
