5fd2e7c530374a0f2fd5c239638c1017d0453f48
cysmix12 <rjsocs@gmail.com>
2022-07-06 02:49:05 +0800

    Added mock APIs for tours and tourGuides

---
+++ src/mock-api/getTourPackages.js
@@ -0,0 +1,41 @@
+export const getTourPackages = async () => {
+  return [
+    {
+      image: '/images/package/p-alpha1.png',
+      date: '2 Day & 1 night',
+      title:
+        'Etiam placerat dictum consequat nullaam an Pellentesque habitant.',
+      price: '$88.00',
+    },
+    {
+      image: '/images/package/p-alpha2.png',
+      date: '4 Day & 3 night',
+      title: 'Varius condimentum consequat nullaam frin Aenean pretium risus.',
+      price: '$98.00',
+    },
+    {
+      image: '/images/package/p-alpha3.png',
+      date: '6 Day & 5 night',
+      title: 'Praesent sed elit mi nullaam. In risus nullaam efficitur non.',
+      price: '$102.00',
+    },
+    {
+      image: '/images/package/p-alpha4.png',
+      date: '5 Day & 4 night',
+      title: 'Sed ultricies sapien arcu, nullaam sed cong feugiat sapien.',
+      price: '$99.50',
+    },
+    {
+      image: '/images/package/p-alpha5.png',
+      date: '6 Day & 5 night',
+      title: 'Pellentesque habitant morbi malesua tristique senectus.',
+      price: '$120.50',
+    },
+    {
+      image: '/images/package/p-alpha6.png',
+      date: '3 Day & 4 night',
+      title: 'San francisco golden gate bridge, golden cable & fog.',
+      price: '$110.50',
+    },
+  ];
+};
